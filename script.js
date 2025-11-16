861a50e2ce121eb092373f629bc22f25f8b0ef2f
Edmond <edmon.digihub@gmail.com>
2025-11-17 00:47:20 +0300

    Remove service worker and install prompt code
    Removed service worker registration and installation prompt logic.

--- script.js
+++ script.js
@@ -286,32 +286,3 @@
             }
         }, 1000);
         
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/service-worker.js")
-    .then(() => console.log("Service Worker Registered"));
-}
-let deferredPrompt;
-const installBtn = document.getElementById('installBtn');
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  installBtn.hidden = false;
-});
-
-installBtn.addEventListener('click', () => {
-  installBtn.hidden = true;
-  deferredPrompt.prompt();
-
-  deferredPrompt.userChoice.then(choice => {
-    console.log(choice.outcome);
-    deferredPrompt = null;
-  });
-});
-
-// Register service worker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
-    .then(() => console.log('Service Worker Registered'))
-    .catch(err => console.log('SW Error:', err));
-}
